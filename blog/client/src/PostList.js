356c2ff996bb0e0021311575ee6fda6f37127bd7
albin-joseph <albinjoseph007@gmail.com>
2024-08-17 07:42:34 +0530

    add cooment create component

--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import axios from 'axios'
+import CommentCreate from "./CommentCreate";
 
 export default () => {
     const [posts, setPosts] = useState({});
@@ -22,6 +23,7 @@ export default () => {
             >
                 <div className="card-body">
                     <h1>{post.title}</h1>
+                    <CommentCreate postId={post.id}/>
                 </div>
             </div>
         )
