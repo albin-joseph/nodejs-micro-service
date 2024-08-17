7f1403be108a263daf0343c8c27111abed6fa28d
albin-joseph <albinjoseph007@gmail.com>
2024-08-17 07:08:05 +0530

    update client app

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -1,9 +1,13 @@
 import Reacat from 'react';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 export default () => {
     return <div className='container'>
         <h1>Create Post</h1>
         <PostCreate />
+        <hr />
+        <h1>Posts</h1>
+        <PostList />
     </div>
 }
