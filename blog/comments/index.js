f60179ce0c92e882282c277b68e6ddb2e2dcbef0
albin-joseph <albinjoseph007@gmail.com>
2024-08-17 18:04:58 +0530

    event bus service implement in posts and comments service

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {randomBytes} = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,12 +14,21 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || [])
 });
 
-app.post('/posts/:id/comments', (req, res)=> {
+app.post('/posts/:id/comments', async (req, res)=> {
     const commentId = randomBytes(4).toLocaleString('hex');
     const {content} = req.body;
     const comments = commentsByPostId[req.params.id] || [];
     comments.push({id: commentId, content});
     commentsByPostId[req.params.id] = comments;
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCrrated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    })
 
     res.status(201).send(comments);
 });
--- blog/posts/index.js
+++ blog/posts/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {randomBytes} = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,12 +14,19 @@ app.get('/posts', (req, res) => {
     res.send(posts)
 });
 
-app.post('/posts', (req, res)=> {
+app.post('/posts', async (req, res)=> {
     const id = randomBytes(4).toLocaleString('hex');
     const {title} = req.body;
     posts[id] = {
         id, title
     };
+
+   await axios.post('http://localhost:4005/events',{
+        type: 'PostCreated',
+        data: {
+            id, title
+        }
+    })
 
     res.status(201).send(posts[id]);
 });
