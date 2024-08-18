bde168c91e8a42cb04f54b0d1ceec5a2b6a949fc
albin-joseph <albinjoseph007@gmail.com>
2024-08-18 14:19:25 +0530

    update client app

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -4,8 +4,19 @@ export default ({comments}) => {
    
 
     const renderedComments = comments.map(comment => {
+        let content;
+            if(comment.status === 'approved'){
+                content = comment.content;
+            }
+
+            if(comment.status === 'pending'){
+                content = 'This comment is awaiting moderation';
+            }
+            if(comment.status === 'rejected'){
+                content = "This comment has been rejected";
+            }
         return(
-            <li key={comment.id}>{comment.content}</li>
+            <li key={comment.id}>{content}</li>
         )
     });
 
