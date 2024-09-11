3851e91ba81b4f8c057655a70e65b9ab4fc5cd15
albin-joseph <albinjoseph007@gmail.com>
2024-09-11 13:52:36 +0530

    add express validator

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,5 +1,5 @@
 import express, {Request, Response} from 'express';
-import { body } from 'express-validator';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
@@ -13,10 +13,14 @@ router.post('/api/users/signup', [
     .withMessage('Password must be between 4 and 20 characters.')
     ],
      (req: Request, res: Response) => {
-    const {email, password} = req.body;
-    if(!email || typeof email !== 'string') {
-        res.send(400).send('Provide a valid email');
-    }
-});
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            return res.status(400).send(errors.array())
+        }
+        const {email, password} = req.body;
+        console.log('Creating a user')
+        res.send({email, password})
+    });
 
 export { router as signupRouter };
