291fea013d48b375c95ec6042a727861f210752b
albin-joseph <albinjoseph007@gmail.com>
2024-09-12 17:17:43 +0530

    configure mongodb and mongoose

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import 'express-async-errors';
 import {json} from 'body-parser';
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -24,6 +25,17 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler);
 
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+        console.log('Connected to mongo DB')
+    } catch(err) {
+        console.error(err)
+    }
+}
+
 app.listen(3000, () => {
     console.log('listerning on port 3000')
 })
+
+start();
