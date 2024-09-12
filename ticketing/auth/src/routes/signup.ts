a5685de52601b559e1ecb5873efafc29e7b23008
albin-joseph <albinjoseph007@gmail.com>
2024-09-12 18:20:30 +0530

    user creation

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, {Request, Response} from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -14,16 +14,25 @@ router.post('/api/users/signup', [
     .isLength({min: 4, max: 20})
     .withMessage('Password must be between 4 and 20 characters.')
     ],
-     (req: Request, res: Response) => {
+     async (req: Request, res: Response) => {
         const errors = validationResult(req);
 
         if(!errors.isEmpty()) {
            throw new RequestValidationError(errors.array())
         }
+
         const {email, password} = req.body;
-        console.log('Creating a user')
-        throw new DatabaseConnectionError()
-        res.send({email, password})
+        const existingUser = await User.findOne({email});
+
+        if(existingUser) {
+            console.log('Email is in use')
+            return res.send({})
+        }
+
+        const user = User.build({email, password});
+        await user.save();
+      
+        res.status(200).send(user)
     });
 
 export { router as signupRouter };
