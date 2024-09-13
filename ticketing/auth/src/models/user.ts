5790fd56709ac90f54d8bc773d8570a52b8f6319
albin-joseph <albinjoseph007@gmail.com>
2024-09-13 09:24:55 +0530

    add hashing password service

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
 
+//An interface that decribe the properties that are required to create a new User
 interface UserAttrs {
     email: string;
     password: string;
 }
 
+//An interface that describes the properties that a User model has
 interface UserModel extends mongoose.Model<UserDoc> {
     build(attrs: UserAttrs): UserDoc;
 }
 
+//An interface that describes the properties that a User Document has
 interface UserDoc extends mongoose.Document {
     email: string;
     password: string;
