00c7fa19f7b34e80105a8a729b84b1393851b00f
albin-joseph <albinjoseph007@gmail.com>
2024-09-19 14:26:34 +0530

    add signout api

--- ticketing/auth/src/routes/signout.ts
+++ ticketing/auth/src/routes/signout.ts
@@ -3,7 +3,8 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-    res.send('Hi there! signout')
+    req.session = null;
+    res.send({})
 });
 
 export { router as signoutRouter };
