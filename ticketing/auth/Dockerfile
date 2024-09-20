798852d40b2d5118c4188c42dc71c6a98e3d904f
albin-joseph <albinjoseph007@gmail.com>
2024-09-20 17:45:16 +0530

    add test env

--- ticketing/auth/Dockerfile
+++ ticketing/auth/Dockerfile
@@ -2,7 +2,7 @@ FROM node:alpine
 
 WORKDIR /app
 COPY package.json .
-RUN npm install
+RUN npm install --omit=dev
 COPY . .
 
 CMD ["npm", "start"]
---
+++ ticketing/auth/src/test/setup.ts
@@ -0,0 +1,29 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+
+let mongo: any;
+beforeAll(async () => {
+    mongo = await MongoMemoryServer.create();
+    const mongoUri = mongo.getUri()
+
+    await mongoose.connect(mongoUri, {});
+
+    beforeEach(async () => {
+        if (mongoose.connection.db) {
+            const collections = await mongoose.connection.db.collections();
+         
+            for (let collection of collections) {
+              await collection.deleteMany({});
+            }
+          }
+    });
+});
+
+afterAll(async () => {
+    if (mongo) {
+      await mongo.stop();
+    }
+    await mongoose.connection.close();
+  });
+  
