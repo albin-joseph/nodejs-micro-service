e3f08beda622c13a22baaa9062f392b49d3d5dfb
albin-joseph <albinjoseph007@gmail.com>
2024-09-23 11:30:25 +0530

    update signup test

--- ticketing/auth/src/routes/__test__/signup.test.ts
+++ ticketing/auth/src/routes/__test__/signup.test.ts
@@ -38,3 +38,21 @@ it('returns a 400 with missing email and password', async () => {
         })
         .expect(400)
 });
+
+it('disallow duplicate emails', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201)
+
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(400)
+});
