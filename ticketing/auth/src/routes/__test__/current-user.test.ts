76a0aa04b289e7b7b251f0bf820c8f37db4ebe98
albin-joseph <albinjoseph007@gmail.com>
2024-09-23 15:18:20 +0530

    add current user test

--- ticketing/auth/src/routes/__test__/current-user.test.ts
+++ ticketing/auth/src/routes/__test__/current-user.test.ts
@@ -2,17 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 
 it('responds with details about the current user', async () => {
-    const authResponse = await request(app)
-    .post('/api/users/signup')
-    .send({
-        email: 'test@test.com',
-        password: 'password'
-    })
-    .expect(201);
-    const cookie = authResponse.get('Set-Cookie');
-    if (!cookie) {
-        throw new Error("Cookie not set after signup");
-    }
+   const cookie = await global.signin();
 
     const response = await request(app)
     .get('/api/users/currentuser')
--- ticketing/auth/src/test/setup.ts
+++ ticketing/auth/src/test/setup.ts
@@ -1,6 +1,11 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
+import  request  from 'supertest';
 import mongoose from 'mongoose';
 import { app } from '../app';
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
 beforeAll(async () => {
@@ -28,4 +33,23 @@ afterAll(async () => {
     }
     await mongoose.connection.close();
   });
+
+  global.signin = async () => {
+      const email = 'test@test.com';
+      const password = 'password';
+
+      const response = await request(app)
+      .post('/api/users/signup')
+      .send({
+        email, password
+      })
+      .expect(201)
+
+      const cookie = response.get("Set-Cookie");
+ 
+      if (!cookie) {
+        throw new Error("Failed to get cookie from response");
+      }
+      return cookie;
+  }
   
