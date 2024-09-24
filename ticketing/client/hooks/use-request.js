25d6dd15a71b5095147e25e532be566f51022530
albin-joseph <albinjoseph007@gmail.com>
2024-09-24 16:29:58 +0530

    add request hook

---
+++ ticketing/client/hooks/use-request.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { useState } from "react";
+
+const useRequest = ({url, method, body}) => {
+    const [errors, setErrors] = useState(null);
+
+    const doRequest = async () => {
+        try {
+           setErrors(null);
+           const response = await axios[method](url, body);
+           return response.data ;
+        }catch(err){
+            setErrors(
+                <div className="alert alert-danger">
+                    <h4>Ooops....</h4>
+                    <ul className="my-0">
+                    {err.response.data.errors.map(err => <li ke={err.message}y>{err.message}</li>)}
+                    </ul>
+                </div>
+            )
+        }
+    }
+
+    return {doRequest, errors}
+}
+
+export default useRequest;
--- ticketing/client/pages/auth/signup.js
+++ ticketing/client/pages/auth/signup.js
@@ -1,23 +1,23 @@
 import { useState } from "react";
 import axios from "axios";
+import useRequest from "../../hooks/use-request";
 
 const SignUp = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState([]);
+    const {doRequest, errors} = useRequest({
+        url: '/api/users/signup',
+        method: 'post',
+        body: {
+            email,
+            password
+        }
+    });
 
     const onSubmit = async (event) => {
         event.preventDefault();
-        try {
-            const response = await axios.post('/api/users/signup', {
-                email,
-                password
-            });
-        }catch(err) {
-            setErrors(err.response.data.errors)
-        }
-
+        doRequest();
     };
 
     return (
@@ -32,12 +32,7 @@ const SignUp = () => {
                     <label>Password</label>
                     <input value={password} onChange={e=>setPassword(e.target.value)} type="password" className="form-control"/>
                 </div>
-                {errors&&errors.length>0&&(<div className="alert alert-danger">
-                    <h4>Ooops....</h4>
-                    <ul className="my-0">
-                    {errors.map(err => <li ke={err.message}y>{err.message}</li>)}
-                    </ul>
-                </div>)}
+                {errors}
                 <button className="btn btn-primary">Sign Up</button>
             </form> 
         </div>
