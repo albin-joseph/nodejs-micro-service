3d43bc66a160eaea9a5f0b160e2421cf5f8f42e4
albin-joseph <albinjoseph007@gmail.com>
2024-09-25 10:59:01 +0530

    update client project

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,7 +1,19 @@
-const LandingPage = () => {
+import axios from "axios";
+
+const LandingPage = ({currentUser}) => {
+    console.log(currentUser);
+   
+    axios.get('/api/users/currentuser').catch((err) => {
+        console.log(err.message);
+      });
+      
     return(
         <h1>Landing Page</h1>
     )
 }
+
+// LandingPage.getInitialProps = () => {
+//     return {}
+// };
 
 export default LandingPage;
