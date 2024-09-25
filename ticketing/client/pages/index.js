4b725c9a2817651cd42bbca9cd92127cc7179aae
albin-joseph <albinjoseph007@gmail.com>
2024-09-25 15:47:59 +0530

    update index page

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,19 +1,24 @@
 import axios from "axios";
 
 const LandingPage = ({currentUser}) => {
-    console.log(currentUser);
+    // console.log(currentUser);
    
-    axios.get('/api/users/currentuser').catch((err) => {
-        console.log(err.message);
-      });
-      
+    // axios.get('/api/users/currentuser').catch((err) => {
+    //     console.log(err.message);
+    //   });
+
     return(
         <h1>Landing Page</h1>
     )
 }
 
-// LandingPage.getInitialProps = () => {
-//     return {}
-// };
+LandingPage.getInitialProps = () => {
+    if(typeof window === 'undefined') {
+        //we are on the server
+    } else {
+        //we are on the browser
+    }
+    return {}
+};
 
 export default LandingPage;
