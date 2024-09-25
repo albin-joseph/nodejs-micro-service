3a9897c7e02ada1796fceefcb7b4ea6c75e67a95
albin-joseph <albinjoseph007@gmail.com>
2024-09-25 18:09:08 +0530

    update initial props config

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,24 +1,33 @@
-import axios from "axios";
+import axios from 'axios';
 
-const LandingPage = ({currentUser}) => {
-    // console.log(currentUser);
-   
-    // axios.get('/api/users/currentuser').catch((err) => {
-    //     console.log(err.message);
-    //   });
+const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser');
+  console.log(currentUser);
 
-    return(
-        <h1>Landing Page</h1>
-    )
-}
+  return <h1>Landing Page</h1>;
+};
 
-LandingPage.getInitialProps = () => {
-    if(typeof window === 'undefined') {
-        //we are on the server
-    } else {
-        //we are on the browser
-    }
-    return {}
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    // we are on the server!
+    // requests should be made to http://ingress-nginx.ingress-nginx...laksdjfk
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    );
+
+    return data;
+  } else {
+    // we are on the browser!
+    // requests can be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
+  }
+  return {};
 };
 
 export default LandingPage;
