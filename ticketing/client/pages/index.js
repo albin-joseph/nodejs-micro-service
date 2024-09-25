a4e4f742fb03648fa542051c07594550dcf81b89
albin-joseph <albinjoseph007@gmail.com>
2024-09-25 18:22:45 +0530

    build client an api client add

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,33 +1,13 @@
-import axios from 'axios';
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  // console.log(currentUser);
-  // axios.get('/api/users/currentuser');
-  console.log(currentUser);
-
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    // we are on the server!
-    // requests should be made to http://ingress-nginx.ingress-nginx...laksdjfk
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers,
-      }
-    );
-
+LandingPage.getInitialProps = async (context) => {
+    const client = buildClient(context)
+    const {data} = await client.get('/api/users/currentuser')
     return data;
-  } else {
-    // we are on the browser!
-    // requests can be made with a base url of ''
-    const { data } = await axios.get('/api/users/currentuser');
-
-    return data;
-  }
-  return {};
 };
 
 export default LandingPage;
