cca51ddee5e4a6abf84394e2c8c5cffe1acbdc84
albin-joseph <albinjoseph007@gmail.com>
2024-09-26 12:58:44 +0530

    update client with login status

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,7 +1,7 @@
 import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  return <h1>Landing Page</h1>;
+  return currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in</h1>
 };
 
 LandingPage.getInitialProps = async (context) => {
