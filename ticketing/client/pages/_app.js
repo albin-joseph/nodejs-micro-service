c12e1fa04427e24d95d48e65475dd6b158e2f518
albin-joseph <albinjoseph007@gmail.com>
2024-09-26 14:19:45 +0530

    app coponent update with inititaial props

--- ticketing/client/pages/_app.js
+++ ticketing/client/pages/_app.js
@@ -1,5 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-export default ({Component, pageProps}) => {
-    return <Component {...pageProps}/>
+const AppComponent = ({Component, pageProps, currentUser}) => {
+    return (
+        <div>
+            <h1>{currentUser.email}</h1>
+            <Component {...pageProps}/>
+        </div>
+)};
+
+
+AppComponent.getInitialProps = async (appContext) => {
+    const client = buildClient(appContext.ctx)
+    const {data} = await client.get('/api/users/currentuser')
+    let pageProps = {}
+    if(appContext.Component.getInitialProps) {
+         pageProps = await appContext.Component.getInitialProps(appContext.ctx)
+    }
+    return {
+        pageProps,
+        ...data
+    };
 };
+
+export default AppComponent;
