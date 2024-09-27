e26a4261b7540ce10e6db16cb8d1e7a4326f6452
albin-joseph <albinjoseph007@gmail.com>
2024-09-27 14:18:13 +0530

    integrate common packages error and middleware move to a npm package

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -2,9 +2,8 @@ import express, {Request, Response} from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
-import { validateRequest } from '../middlewares/validate-request';
+import { validateRequest, BadRequestError } from '@ajauthticket/common';
 import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
