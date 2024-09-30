e607bbfd025cf3f8de33d7d43b9a3f3840f0a461
albin-joseph <albinjoseph007@gmail.com>
2024-09-30 13:26:07 +0530

    add auth protection

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import 'express-async-errors';
 import {json} from 'body-parser';
 import cookieSession from "cookie-session";
-import { errorHandler, NotFoundError } from "@ajauthticket/common";
+import { errorHandler, NotFoundError, currentUser } from "@ajauthticket/common";
 
 import {createTicketRouter} from './routes/new';
 
@@ -14,6 +14,8 @@ app.use(cookieSession({
     signed: false,
     secure: process.env.NODE_ENV !== 'test'
 }));
+
+app.use(currentUser);
 
 app.use(createTicketRouter);
 
--- ticketing/tickets/src/routes/__test__/new.test.ts
+++ ticketing/tickets/src/routes/__test__/new.test.ts
@@ -10,7 +10,19 @@ it('has a route handler listening to /api/tickets for post request', async () =>
 });
 
 it('can only be access if the user is signed in', async () => {
+    const response = await request(app)
+    .post('/api/tickets')
+    .send({});
 
+    expect(response.status).toEqual(401);
+});
+
+it('returns a status other than 401 if the user is signed in', async () => {
+    const response = await request(app)
+    .post('/api/tickets')
+    .send({});
+
+    expect(response.status).not.toEqual(401);
 });
 
 it('returns an error if an invalid title is provided', async () => {
