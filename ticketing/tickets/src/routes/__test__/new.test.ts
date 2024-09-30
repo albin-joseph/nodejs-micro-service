9829349568f8e112d4df24576ddea8cbfe07ef19
albin-joseph <albinjoseph007@gmail.com>
2024-09-30 12:09:06 +0530

    add tickets route tests

---
+++ ticketing/tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,22 @@
+import { Request } from "supertest";
+import { app } from "../../app";
+
+it('has a route handler listening to /api/tickets for post request', async () => {
+
+});
+
+it('can only be access if the user is signed in', async () => {
+
+});
+
+it('returns an error if an invalid title is provided', async () => {
+
+});
+
+it('returns an error if an invalid price is provided', async () => {
+
+});
+
+it('create a ticket with valid inputs', async () => {
+
+});
