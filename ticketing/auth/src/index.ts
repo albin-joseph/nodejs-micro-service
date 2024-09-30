2b683758b820bd90e7dde1ee7edc76b7374de22e
albin-joseph <albinjoseph007@gmail.com>
2024-09-30 11:58:36 +0530

    mongo uri move to k8s deploymemnt file

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -5,8 +5,11 @@ const start = async () => {
     if(!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined')
     }
+    if(!process.env.MONGO_URI) {
+        throw new Error('MONGO_URI must be defined')
+    }
     try {
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+        await mongoose.connect(process.env.MONGO_URI)
         console.log('Connected to mongo DB')
     } catch(err) {
         console.error(err)
