fa19cce2e77d6825deb5772056fb1457117c3311
albin-joseph <albinjoseph007@gmail.com>
2024-10-01 16:33:25 +0530

    add input validation

--- ticketing/tickets/src/routes/new.ts
+++ ticketing/tickets/src/routes/new.ts
@@ -1,9 +1,17 @@
 import express, {Request, Response} from 'express';
-import {requireAuth} from '@ajauthticket/common';
+import {body} from 'express-validator';
+import {requireAuth, validateRequest} from '@ajauthticket/common';
 
 const router = express.Router();
 
-router.post('/api/tickets', [requireAuth] ,(req: Request, res: Response) => {
+router.post('/api/tickets', [requireAuth] ,[
+    body('title')
+    .notEmpty()
+    .withMessage('Title is required'),
+    body('price')
+    .isFloat({gt:0})
+    .withMessage('Price must be greater than zero')
+], validateRequest,(req: Request, res: Response) => {
     res.sendStatus(200);
 });
 
