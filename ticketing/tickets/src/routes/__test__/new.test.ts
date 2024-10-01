20e610b08a622dd7574d702dd71fbcdba80806fe
albin-joseph <albinjoseph007@gmail.com>
2024-10-01 16:26:11 +0530

    update unitest

--- ticketing/tickets/src/routes/__test__/new.test.ts
+++ ticketing/tickets/src/routes/__test__/new.test.ts
@@ -27,11 +27,41 @@ it('returns a status other than 401 if the user is signed in', async () => {
 });
 
 it('returns an error if an invalid title is provided', async () => {
+    await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+        title: '',
+        price: 10
+    })
+    .expect(400);
 
+    await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+        price: 10
+    })
+    .expect(400);
 });
 
 it('returns an error if an invalid price is provided', async () => {
+    await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+        title: 'asdfghjk',
+        price: -10
+    })
+    .expect(400);
 
+    await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+        title: 'asdfgh'
+    })
+    .expect(400);
 });
 
 it('create a ticket with valid inputs', async () => {
