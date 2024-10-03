1ecefe8de09b01324f83a36db486cc90a84a424f
albin-joseph <albinjoseph007@gmail.com>
2024-10-03 11:59:48 +0530

    add ticket show router

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -5,6 +5,7 @@ import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@ajauthticket/common";
 
 import {createTicketRouter} from './routes/new';
+import { showTicketRouter } from "./routes/show";
 
 
 const app = express();
@@ -18,6 +19,7 @@ app.use(cookieSession({
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError();
