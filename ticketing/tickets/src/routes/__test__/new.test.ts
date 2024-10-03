4af3340bf995a649681e51ac5e49eebf28eb16f2
albin-joseph <albinjoseph007@gmail.com>
2024-10-03 10:32:22 +0530

    update ticket create unit test and route

--- ticketing/tickets/src/routes/__test__/new.test.ts
+++ ticketing/tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
 
 it('has a route handler listening to /api/tickets for post request', async () => {
     const response = await request(app)
@@ -65,11 +66,20 @@ it('returns an error if an invalid price is provided', async () => {
 });
 
 it('create a ticket with valid inputs', async () => {
+    let tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(0);
+    const title = 'asdfghj';
     await request(app)
     .post('/api/tickets')
+    .set('Cookie', global.signin())
     .send({
-        title: 'asdfghj',
-        print: 10
+        title,
+        price: 20
     })
     .expect(201)
+
+    tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(1);
+    expect(tickets[0].price).toEqual(20);
+    expect(tickets[0].title).toEqual(title);
 });
--- ticketing/tickets/src/routes/new.ts
+++ ticketing/tickets/src/routes/new.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from 'express';
 import {body} from 'express-validator';
 import {requireAuth, validateRequest} from '@ajauthticket/common';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
@@ -11,8 +12,15 @@ router.post('/api/tickets', [requireAuth] ,[
     body('price')
     .isFloat({gt:0})
     .withMessage('Price must be greater than zero')
-], validateRequest,(req: Request, res: Response) => {
-    res.sendStatus(200);
+], validateRequest, async (req: Request, res: Response) => {
+    const {title, price} = req.body;
+    const ticket = Ticket.build({
+        title,
+        price,
+        userId: req.currentUser!.id
+    });
+    await ticket.save();
+    res.status(201).send(ticket);
 });
 
 export { router as createTicketRouter}
