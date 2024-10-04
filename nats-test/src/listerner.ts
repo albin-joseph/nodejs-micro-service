c493ff8821f6c903defc94c4fdea617c6ffbfa77
albin-joseph <albinjoseph007@gmail.com>
2024-10-04 17:14:02 +0530

    update listerner and publisher

--- nats-test/src/listerner.ts
+++ nats-test/src/listerner.ts
@@ -1,38 +1,36 @@
-import nats, {Message} from 'node-nats-streaming';
+import nats, { Message } from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
 
 console.clear();
 
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
-    url: 'http://localhost:4222'
+  url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
-    console.log('Listerner connect to NATS');
+  console.log('Listener connected to NATS');
 
-    stan.on('close', () => {
-        console.log('NATS connection closed!');
-        process.exit()
-    })
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
 
-    const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true);
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'orders-service-queue-group',
+    options
+  );
 
-    const subscription = stan.subscribe(
-        'ticket:created', 
-        'orders-service-queue-group',
-        options
-    );
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
 
-    subscription.on('message', (msg: Message) =>{
-        const data = msg.getData();
-        if(typeof data === 'string') {
-            console.log(`Recieved event #${msg.getSequence()}, with data: ${data}`)
-        }
+    if (typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
 
-        msg.ack();
-    });
+    msg.ack();
+  });
 });
 
 process.on('SIGINT', () => stan.close());
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,18 +1,21 @@
 import nats from 'node-nats-streaming';
 
+console.clear();
+
 const stan = nats.connect('ticketing', 'abc', {
-    url: 'http://localhost:4222'
+  url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
-    console.log('Publisher connected to NATS');
-    const data = JSON.stringify({
-        id: '123',
-        title: 'concert',
-        price: 20
-    });
+  console.log('Publisher connected to NATS');
 
-    stan.publish('ticket:created', data, () => {
-        console.log('Event published');
-    })
+  const data = JSON.stringify({
+    id: '123',
+    title: 'concert',
+    price: 20,
+  });
+
+  stan.publish('ticket:created', data, () => {
+    console.log('Event published');
+  });
 });
