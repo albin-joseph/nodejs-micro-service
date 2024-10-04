c013593ca48faac0ca3995e060f6986bfefd7240
albin-joseph <albinjoseph007@gmail.com>
2024-10-04 17:08:00 +0530

    update listerner

--- nats-test/src/listerner.ts
+++ nats-test/src/listerner.ts
@@ -10,12 +10,30 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
     console.log('Listerner connect to NATS');
 
-    const subscription = stan.subscribe('ticket:created', 'orders-service-queue-group');
+    stan.on('close', () => {
+        console.log('NATS connection closed!');
+        process.exit()
+    })
+
+    const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true);
+
+    const subscription = stan.subscribe(
+        'ticket:created', 
+        'orders-service-queue-group',
+        options
+    );
 
     subscription.on('message', (msg: Message) =>{
         const data = msg.getData();
         if(typeof data === 'string') {
             console.log(`Recieved event #${msg.getSequence()}, with data: ${data}`)
         }
-    })
-})
+
+        msg.ack();
+    });
+});
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
