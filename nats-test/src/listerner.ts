73e6abb4ee8849a69639b0d68656d7e270f9c694
albin-joseph <albinjoseph007@gmail.com>
2024-10-10 16:04:54 +0530

    add abstract class listerner

--- nats-test/src/listerner.ts
+++ nats-test/src/listerner.ts
@@ -15,26 +15,7 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan
-  .subscriptionOptions()
-  .setManualAckMode(true)
-  .setDeliverAllAvailable()
-  .setDurableName('accounting-service');
-  const subscription = stan.subscribe(
-    'ticket:created',
-    'orders-service-queue-group',
-    options
-  );
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === 'string') {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
-
-    msg.ack();
-  });
+    new TicketCreatedListener(stan).listen();
 });
 
 process.on('SIGINT', () => stan.close());
@@ -83,4 +64,16 @@ abstract class Listener {
       ? JSON.parse(data)
       : JSON.parse(data.toString('utf-8'));
   }
+}
+
+class TicketCreatedListener extends Listener {
+  subject = 'ticket:created';
+  queGroupName = 'payments-service';
+
+  onMessage(data: any, msg: Message): void {
+    console.log('Event data:', data);
+
+    msg.ack();
+  }
+  
 }
