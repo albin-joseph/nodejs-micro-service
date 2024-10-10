4787fac9f331a93a7b01e1bd2665f3d86b1f485f
albin-joseph <albinjoseph007@gmail.com>
2024-10-10 15:50:47 +0530

    add listern abstract class

--- nats-test/src/listerner.ts
+++ nats-test/src/listerner.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats, { Message, Stan } from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
 
 console.clear();
@@ -39,3 +39,48 @@ stan.on('connect', () => {
 
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
+
+abstract class Listener {
+  abstract subject: string;
+  abstract queGroupName: string;
+  abstract onMessage(data: any, msg: Message): void;
+  private  client: Stan;
+  protected ackWait = 5 * 1000;
+
+  constructor(client: Stan) {
+      this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+    .subscriptionOptions()
+    .setDeliverAllAvailable()
+    .setManualAckMode(true)
+    .setAckWait(this.ackWait)
+    .setDurableName(this.queGroupName);
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on('message', (msg: Message) => {
+      console.log(
+        `Message received ${this.subject}/${this.queGroupName}`
+      );
+
+      const parsedData = this.parseMessage(msg)
+      this.onMessage(parsedData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === 'string'
+      ? JSON.parse(data)
+      : JSON.parse(data.toString('utf-8'));
+  }
+}
