144d91f799f9a6ca9b4323683c3d71b7f5a3aa6a
albin-joseph <albinjoseph007@gmail.com>
2024-10-11 17:58:23 +0530

    integrate NATS client

--- ticketing/tickets/src/index.ts
+++ ticketing/tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
     if(!process.env.JWT_KEY) {
@@ -9,6 +10,7 @@ const start = async () => {
         throw new Error('MONGO_URI must be defined')
     }
     try {
+        await natsWrapper.connect('ticketing', 'asdfghjk', 'http://nats-srv:4222')
         await mongoose.connect(process.env.MONGO_URI)
         console.log('Connected to mongo DB')
     } catch(err) {
---
+++ ticketing/tickets/src/nats-wrapper.ts
@@ -0,0 +1,31 @@
+import nats, {Stan} from 'node-nats-streaming';
+
+class NatsWrapper {
+    private _client?: Stan;
+
+    get client() {
+        if(!this._client) {
+            throw new Error('Cannot access NATS client before connecting');
+        }
+
+        return this._client;
+    }
+
+    connect(clusterId: string, clientId: string, url: string) {
+        this._client = nats.connect(clusterId, clientId, {url});
+
+        return new Promise<void>((resolve, reject) => {
+            this._client!.on('connect', () => {
+                console.log('Connected to NATS');
+                resolve();
+            });
+
+            this._client!.on('error', (err) => {
+                reject(err);
+            });
+
+        })
+    }
+}
+
+export const natsWrapper = new NatsWrapper();
