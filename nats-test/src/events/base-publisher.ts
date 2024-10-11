8b3a174758a0aa21991cfbacab0312af2d0d26b3
albin-joseph <albinjoseph007@gmail.com>
2024-10-11 10:37:19 +0530

    update base publisher

--- nats-test/src/events/base-publisher.ts
+++ nats-test/src/events/base-publisher.ts
@@ -20,6 +20,7 @@ export abstract class Publisher<T extends Event> {
                 if(err) {
                     return reject(err);
                 }
+                console.log('Event published to subject', this.subject);
                 resolve()
             });
         } )
