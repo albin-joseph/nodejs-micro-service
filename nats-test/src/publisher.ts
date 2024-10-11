50750e5d0a754b6dd957a9827552cd386305975c
albin-joseph <albinjoseph007@gmail.com>
2024-10-11 09:18:46 +0530

    add base publisher and ticket created piblisher subclass

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,7 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
+
+
 
 console.clear();
 
@@ -8,14 +11,20 @@ const stan = nats.connect('ticketing', 'abc', {
 
 stan.on('connect', () => {
   console.log('Publisher connected to NATS');
-
-  const data = JSON.stringify({
+  const publisher = new TicketCreatedPublisher(stan);
+  publisher.publish({
     id: '123',
     title: 'concert',
     price: 20,
-  });
+  })
 
-  stan.publish('ticket:created', data, () => {
-    console.log('Event published');
-  });
+  // const data = JSON.stringify({
+  //   id: '123',
+  //   title: 'concert',
+  //   price: 20,
+  // });
+
+  // stan.publish('ticket:created', data, () => {
+  //   console.log('Event published');
+  // });
 });
