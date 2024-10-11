e86b2a8b9c2df1284eb71187ad410a0845a447dc
albin-joseph <albinjoseph007@gmail.com>
2024-10-11 09:24:14 +0530

    add async publisher

--- nats-test/src/events/base-publisher.ts
+++ nats-test/src/events/base-publisher.ts
@@ -14,9 +14,14 @@ export abstract class Publisher<T extends Event> {
         this.client = client;
     }
 
-    publish(data: T['data']) {
-        this.client.publish(this.subject, JSON.stringify(data), () => {
-            console.log('Event pusblished');
-        });
+    publish(data: T['data']): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.client.publish(this.subject, JSON.stringify(data), (err) => {
+                if(err) {
+                    return reject(err);
+                }
+                resolve()
+            });
+        } )
     }
 }
