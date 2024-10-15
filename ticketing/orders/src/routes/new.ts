a84531dfb4bcb74846a60623fd48303c6260f955
albin-joseph <albinjoseph007@gmail.com>
2024-10-15 17:35:56 +0530

    add order creation logic

--- ticketing/orders/src/routes/new.ts
+++ ticketing/orders/src/routes/new.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 import express, {Request, Response} from 'express';
 import {body} from 'express-validator';
-import {requireAuth, validateRequest} from '@ajauthticket/common';
+import {NotFoundError, requireAuth, validateRequest} from '@ajauthticket/common';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -12,6 +14,17 @@ router.post('/api/orders', requireAuth, [
     .custom((input:string)=> mongoose.Types.ObjectId.isValid(input))
     .withMessage('TicketId must be provided')
 ],validateRequest, async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    //Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if(!ticket) {
+        throw new NotFoundError();
+    }
+    //Make sure that the ticket is not already reserved
+    //Calculate an expiration date for this order
+    //Build the order and save it to the database
+    //Publish an event saying that an ordr was created
     res.send({});
 });
 
