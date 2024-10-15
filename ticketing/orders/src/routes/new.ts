32e3ec1d2de04e37a5e405ebcd303c36ce81cb62
albin-joseph <albinjoseph007@gmail.com>
2024-10-15 18:14:59 +0530

    create order service update

--- ticketing/orders/src/routes/new.ts
+++ ticketing/orders/src/routes/new.ts
@@ -7,6 +7,8 @@ import { Order } from '../models/order';
 
 const router = express.Router();
 
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
 router.post('/api/orders', requireAuth, [
    body('ticketId')
     .not()
@@ -29,10 +31,21 @@ router.post('/api/orders', requireAuth, [
     if(isReserved) {
         throw new BadRequestError('Ticket is already reserved.');
     }
+
     //Calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
     //Build the order and save it to the database
+    const order = Order.build({
+        userId: req.currentUser!.id,
+        status: OrderStatus.Created,
+        expiresAt: expiration,
+        ticket: ticket
+    });
+    await order.save();
     //Publish an event saying that an ordr was created
-    res.send({});
+    res.status(201).send(order);
 });
 
 export { router as newOrderRouter };
