191685b20db32de3a817d8678deac704dd186048
albin-joseph <albinjoseph007@gmail.com>
2024-10-16 11:07:10 +0530

    delete order route add

--- ticketing/orders/src/routes/delete.ts
+++ ticketing/orders/src/routes/delete.ts
@@ -1,9 +1,25 @@
 import express, {Request, Response} from "express";
+import { NotAutherizedError, NotFoundError, requireAuth } from "@ajauthticket/common";
+import { Order, OrderStatus } from "../models/order";
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-    res.send({});
+router.delete('/api/orders/:orderId',requireAuth ,async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if(!order) {
+        throw new NotFoundError();
+    }
+    if(order.userId !== req.currentUser!.id) {
+        throw new NotAutherizedError()
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    res.status(204).send(order);
 });
 
 export { router as deleteOrderRouter };
