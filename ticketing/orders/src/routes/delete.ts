fba0a0cc053077004883aa8f4444df3fa85fc7c1
albin-joseph <albinjoseph007@gmail.com>
2024-10-17 10:08:48 +0530

    add nats order created publisher and order cancelled publisher and unit tests

--- ticketing/orders/src/routes/delete.ts
+++ ticketing/orders/src/routes/delete.ts
@@ -1,13 +1,15 @@
 import express, {Request, Response} from "express";
 import { NotAutherizedError, NotFoundError, requireAuth } from "@ajauthticket/common";
 import { Order, OrderStatus } from "../models/order";
+import { OrderCancelledPublisher } from "../events/publishers/order-cancelled-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
 router.delete('/api/orders/:orderId',requireAuth ,async (req: Request, res: Response) => {
     const { orderId } = req.params;
 
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate('ticket');
 
     if(!order) {
         throw new NotFoundError();
@@ -20,6 +22,12 @@ router.delete('/api/orders/:orderId',requireAuth ,async (req: Request, res: Resp
     await order.save();
 
     //publishing an event saying this was cancelled!
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+        id: order.id,
+        ticket: {
+            id: order.ticket.id
+        }
+    })
 
     res.status(204).send(order);
 });
