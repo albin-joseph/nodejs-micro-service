352a82e8a2661cae3dc1460a8008ed693f669c45
albin-joseph <albinjoseph007@gmail.com>
2024-10-21 13:47:16 +0530

    update listners test

---
+++ ticketing/orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,20 @@
+const setup = async () => {
+    //create an instance of the listener
+
+    //create a fake data event
+
+    //create a fake message object
+};
+
+it('creates and saves a ticket', async () => {
+    //call the onMessage function with the data object + message object
+
+    //write assertion to make sure a ticket was created!
+
+});
+
+it('ack the message', async () => {
+    //call the onMessage function with the data object + message object
+
+    //write assertion to make sure ack function is called
+});
