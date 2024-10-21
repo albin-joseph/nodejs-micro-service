55abef5c4ba73f2e9ccabc57a0b79678b37f121b
albin-joseph <albinjoseph007@gmail.com>
2024-10-21 17:09:33 +0530

    update ticket service

--- ticketing/tickets/src/events/listeners/order-created-listener.ts
+++ ticketing/tickets/src/events/listeners/order-created-listener.ts
@@ -1,13 +1,29 @@
 import { Message } from "node-nats-streaming";
 import { Listener, OrderCreatedEvent, Subjects } from "@ajauthticket/common";
 import { queueGroupName } from "./queue-group-name";
+import { Ticket } from "../../models/ticket";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     subject: Subjects.OrderCreated = Subjects.OrderCreated;
     queGroupName = queueGroupName;
 
     async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-        
+        //Find the ticket that the order is reserving
+        const ticket = await Ticket.findById(data.ticket.id);
+
+        //If no ticket, throw error
+        if(!ticket) {
+            throw new Error('Ticket not found');
+        }
+
+        //Mark the ticket as being reserved by setting its order property 
+        ticket.set({orderId: data.id});
+
+        //Save the ticket
+        await ticket.save();
+
+        //ack the message
+        msg.ack();
     }
 
 }
