954040469a01ee7e88887ac1d9e2bd6b25e05850
albin-joseph <albinjoseph007@gmail.com>
2024-10-22 14:07:44 +0530

    add listerners to ticket service

--- ticketing/tickets/src/index.ts
+++ ticketing/tickets/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import { app } from "./app";
 import { natsWrapper } from "./nats-wrapper";
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
+import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
 
 const start = async () => {
     if(!process.env.JWT_KEY) {
@@ -33,6 +35,9 @@ const start = async () => {
 
         process.on('SIGINT', () => natsWrapper.client.close());
         process.on('SIGTERM', () => natsWrapper.client.close());
+
+        new OrderCreatedListener(natsWrapper.client).listen();
+        new OrderCancelledListener(natsWrapper.client).listen();
 
         await mongoose.connect(process.env.MONGO_URI)
         console.log('Connected to mongo DB')
