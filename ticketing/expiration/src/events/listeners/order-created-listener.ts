b818c9408445ea744abf483163e8582b14b7be0d
albin-joseph <albinjoseph007@gmail.com>
2024-10-24 09:44:50 +0530

    add delay fro process expiration

--- ticketing/expiration/src/events/listeners/order-created-listener.ts
+++ ticketing/expiration/src/events/listeners/order-created-listener.ts
@@ -7,8 +7,13 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
     subject: Subjects.OrderCreated = Subjects.OrderCreated
     queGroupName = queueGroupName;
    async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+        const delay = new Date(data.expiresAt).getTime()- new Date().getTime();
+        console.log('Waiting this many milliseconds to process the job:', delay);
+
         await expirationQueue.add({
             orderId: data.id
+        }, {
+            delay
         });
 
         msg.ack();
