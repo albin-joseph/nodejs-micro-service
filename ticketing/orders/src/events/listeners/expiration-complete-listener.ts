63edf1f05dca490202791e40473770d7db3c9f36
albin-joseph <albinjoseph007@gmail.com>
2024-10-24 11:59:15 +0530

    expiration complete listner add

---
+++ ticketing/orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,32 @@
+import { Listener, Subjects, ExpirationCompleteEvent, OrderStatus } from "@ajauthticket/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+import { Order } from "../../models/order";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
+
+export class ExpirationCompleteListner extends Listener<ExpirationCompleteEvent>{
+    subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+    queGroupName = queueGroupName;
+    async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+        const order = await Order.findById(data.orderId).populate('ticket');
+
+        if(!order) {
+            throw new Error('Order not found');
+        }
+
+        order.set({
+            status: OrderStatus.Cancelled
+        });
+
+        await order.save();
+        new OrderCancelledPublisher(this.client).publish({
+            id: order.id,
+            version: order.version,
+            ticket: {
+                id: order.ticket.id
+            }
+        })
+        msg.ack();
+    }
+
+}
