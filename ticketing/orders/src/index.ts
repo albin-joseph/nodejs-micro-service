ad1ba5a8c8b2cb657d40824d43cdbf694b1de380
albin-joseph <albinjoseph007@gmail.com>
2024-10-24 12:23:21 +0530

    expiration listner unit test add

--- ticketing/orders/src/index.ts
+++ ticketing/orders/src/index.ts
@@ -3,6 +3,7 @@ import { app } from "./app";
 import { natsWrapper } from "./nats-wrapper";
 import { TicketCreatedListener } from "./events/listeners/ticket-created-listener";
 import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
+import { ExpirationCompleteListner } from "./events/listeners/expiration-complete-listener";
 
 const start = async () => {
     if(!process.env.JWT_KEY) {
@@ -38,6 +39,7 @@ const start = async () => {
 
         new TicketCreatedListener(natsWrapper.client).listen();
         new TicketUpdatedListener(natsWrapper.client).listen();
+        new ExpirationCompleteListner(natsWrapper.client).listen();
 
         await mongoose.connect(process.env.MONGO_URI)
         console.log('Connected to mongo DB')
