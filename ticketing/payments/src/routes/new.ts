1072d5ed931b7a7f05971da1d5ae9c3d1ddad18f
albin-joseph <albinjoseph007@gmail.com>
2024-10-25 12:00:23 +0530

    add new charge

--- ticketing/payments/src/routes/new.ts
+++ ticketing/payments/src/routes/new.ts
@@ -4,21 +4,39 @@ import {
      requireAuth,
      validateRequest,
      BadRequestError,
-     NotFoundError
+     NotFoundError,
+     NotAutherizedError,
+     OrderStatus
 } from '@ajauthticket/common';
 
 import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.post('/api/payaments', requireAuth, [
+router.post('/api/payments', requireAuth, [
     body('token')
     .not()
     .isEmpty(),
     body('orderId')
     .not()
     .isEmpty()
-], validateRequest,(req: Request, res: Response) => {
+], validateRequest, async (req: Request, res: Response) => {
+    const {token, orderId} = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if(!order) {
+        throw new NotFoundError();
+    }
+
+    if(order.userId !== req.currentUser!.id) {
+        throw new NotAutherizedError();
+    }
+
+    if(order.status === OrderStatus.Cancelled) {
+        throw new BadRequestError('Cannot pay for a cancelled order');
+    }
+
     res.send({Success: true})
 });
 
