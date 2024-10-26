7c734138f8d9a4bc7df32945e3f822393c88933a
albin-joseph <albinjoseph007@gmail.com>
2024-10-26 07:35:00 +0530

    update stripe integration

--- ticketing/payments/src/routes/new.ts
+++ ticketing/payments/src/routes/new.ts
@@ -8,8 +8,8 @@ import {
      NotAutherizedError,
      OrderStatus
 } from '@ajauthticket/common';
-
 import { Order } from '../models/order';
+import { stripe } from '../stripe';
 
 const router = express.Router();
 
@@ -36,6 +36,12 @@ router.post('/api/payments', requireAuth, [
     if(order.status === OrderStatus.Cancelled) {
         throw new BadRequestError('Cannot pay for a cancelled order');
     }
+
+    await stripe.charges.create({
+        currency: 'usd',
+        amount: order.price * 100,
+        source: token,
+    })
 
     res.send({Success: true})
 });
