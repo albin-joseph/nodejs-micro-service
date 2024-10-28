1f707ae94276a4005b7483227c9d00358841108a
albin-joseph <albinjoseph007@gmail.com>
2024-10-28 09:24:35 +0530

    add payment created publisher

--- ticketing/payments/src/routes/new.ts
+++ ticketing/payments/src/routes/new.ts
@@ -11,6 +11,8 @@ import {
 import { Order } from '../models/order';
 import { Payment } from '../models/payment';
 import { stripe } from '../stripe';
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -51,7 +53,13 @@ router.post('/api/payments', requireAuth, [
 
     await payment.save();
 
-    res.status(201).send({Success: true})
+    await new PaymentCreatedPublisher(natsWrapper.client).publish({
+        id: payment.id,
+        orderId: payment.orderId,
+        stripeId: payment.stripeId
+    });
+
+    res.status(201).send({id: payment.id})
 });
 
 export {router as createChargeRouter}
