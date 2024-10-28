493f3acc29ffac82ca7ee4461265fc1ef56a1410
albin-joseph <albinjoseph007@gmail.com>
2024-10-28 16:02:58 +0530

    add expiry logic in client

--- ticketing/client/pages/orders/[orderId].js
+++ ticketing/client/pages/orders/[orderId].js
@@ -1,9 +1,36 @@
-const OrderShow = () => {
+import {useEffect, useState} from 'react'
+const OrderShow = ({order}) => {
+
+    const [timeLeft, setTimeLeft] = useState('');
+
+    useEffect(()=>{
+        const findTimeLeft = () => {
+            const msLeft = new Date(order.expiresAt) - new Date();
+            setTimeLeft(Math.round(msLeft/1000))
+        }
+        findTimeLeft();
+        const timerId = setInterval(findTimeLeft, 1000);
+
+        return () => {
+            clearInterval(timerId);
+        }
+    },[order]);
+
+    if(timeLeft<0) {
+        return <div>Order expired</div>
+    }
+
     return (
         <div>
-            Order Show
+            <div>Tme letf to pay: {timeLeft} seconds</div>
         </div>
     )
 }
+
+OrderShow.getInitialProps = async (context, client) => {
+    const {orderId} = context.query;
+    const {data} = await client.get(`/api/orders/${orderId}`);
+    return {order: data};
+};
 
 export default OrderShow;
